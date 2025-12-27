c5488156010734ee481c533c902d41bb3c445ab2
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-12-27 09:49:12 +0530

    welcome page and layout page completed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
+import Layout from "./components/layouts/Layout.vue";
+import Welcome from "./components/pages/Welcome.vue";
 </script>
 
 <template>
-
+  <Layout>
+    <Welcome />
+  </Layout>
 </template>
